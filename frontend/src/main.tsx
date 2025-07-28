a420c9fa87c7069ba6f55dcdb6e1769d49771ba9
AtCloudDiwash <diwash.kuskusmiya@gmail.com>
2025-07-28 07:30:45 -0400

    fix routing

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import WavyParticles from './components/ui/wavyparticles.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import WavyParticles from "./components/ui/wavyparticles.tsx";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
+  <Router>
     <WavyParticles>
-        <App />
+      <Routes>
+        <Route path="/landing" element={<App/>}></Route>
+        <Route path="*" element={<Navigate to="/landing" replace/>}/>
+      </Routes>
     </WavyParticles>
-)
+  </Router>
+);
